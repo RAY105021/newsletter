f2ed2d64f3c93730978ac6e5b7c5419b46cc2e85
ray.jy.huang <ray.jy.huang@lasertek.com.tw>
2022-03-17 16:17:32 +0800

    newsletter_01

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<title>雷科股份有限公司-入口網站</title>
+<meta name="description" content="Blueprint: Responsive Full Width Grid Layout" />
+<meta name="keywords" content="100% grid, layout, columns, images, thumbnails, responsive, full width grid, image grid, css, jquery" />
+<meta name="author" content="Codrops" />
+<style>
+section {
+  width: 500px;
+  height: 300px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -150px 0 0 -250px;
+  display: table;
+}
+section .text-center {
+  display: table-cell;
+  vertical-align: middle;
+}
+h2, h3, p {
+  text-align: center;
+}
+</style>
+
+</head>
+<body>
+
+<section>
+  <div class="text-center">
+    <h2>新版雷科入網站Portal，已正式上線。</h2>
+    <div class="data-wrap">
+      <h3><a href="http://portal.lasertek.com.tw:8081/" target="_blank">點我前往</a>（請用ＩＥ瀏覽器）</h3>
+      <p>舊版雷科入口網站，已正式關閉！！若有問題，請再聯絡ＭＩＳ</p>
+    </div>
+  </div>
+</section>
+
+</body>
+</html>
